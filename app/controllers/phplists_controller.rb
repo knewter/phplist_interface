be92698c39892875bebc6f700dad858a2fd70861
Josh Adams <jadams@goro.(none)>
2009-04-16 13:44:35 -0500

    added ability to show a given list, oops

--- app/controllers/phplists_controller.rb
+++ app/controllers/phplists_controller.rb
@@ -1,15 +1,26 @@
 class PhplistsController < ApplicationController
   before_filter :load_phplists, :only => [:index]
+  before_filter :load_phplist, :only => [:show]
 
   protected
   def load_phplists
     @phplists = Phplist.find(:all)
   end
 
+  def load_phplist
+    @phplist = Phplist.find(params[:id])
+  end
+
   public
   def index
     respond_to do |format|
       format.xml{ render :xml => @phplists }
+    end
+  end
+
+  def show
+    respond_to do |format|
+      format.xml{ render :xml => @phplist }
     end
   end
 end
